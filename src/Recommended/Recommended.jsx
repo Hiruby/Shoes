ec854d90d8df1c68da502bcde64bd12d69884b27
Hiruby <truongduchai4@gmail.com>
2025-04-10 23:28:58 +0700

    Products updated

--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Recommended.css'
+import "../../index.css"
 
 const Recommended = () => {
-  return (
+  return <>
     <div>
-      Recommended
+      <h2 className="recommended-title">Recommended</h2>
+      <div className="recommended-flex">
+        <button className="btns">All Products</button>
+        <button className="btns">Nike</button>
+        <button className="btns">Adidas</button>
+        <button className="btns">Puma</button>
+        <button className="btns">Vans</button>
+      </div>
     </div>
-  )
+  </>
 }
 
 export default Recommended
